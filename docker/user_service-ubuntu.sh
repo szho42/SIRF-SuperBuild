19d543b4c0e9429cf5fbcb533f2a0916ba3f1c24
Casper da Costa-Luis <casper.dcl@physics.org>
2019-07-23 00:41:39 +0100

    more wip

--- docker/user_service-ubuntu.sh
+++ docker/user_service-ubuntu.sh
@@ -6,18 +6,17 @@ INSTALL_DIR="${1:-/opt}"
 # SIRF-Exercises
 git clone https://github.com/CCPPETMR/SIRF-Exercises --recursive -b master $INSTALL_DIR/SIRF-Exercises
 
-if [ -f requirements-service.txt ]; then
-  conda install -c conda-forge -y --file requirements-service.txt || \
-  pip install -U -r requirements-service.txt
-fi
+conda install -c conda-forge -y ipykernel ipywidgets && conda clean -y --all|| \
+  pip install --no-cache-dir ipykernel ipywidgets
+pip install --no-cache-dir keras tensorboard tensorflow-gpu
 python -m ipykernel install --name sirf --display-name "Python (sirf)"
 
 if [ -f requirements-hub.txt ]; then
-  ( set -e
-    conda create -n hub -c conda-forge -y --file requirements-hub.txt python=3
-    conda activate hub
-  ) || \
-  pip3 install -U -r requirements-hub.txt
+  conda create -n hub -c conda-forge -y python=3
+  conda activate hub
+  conda install -c conda-forge -y --file requirements-hub.txt
+  conda clean -y --all
+  #pip3 install --no-cache-dir -r requirements-hub.txt
 
   jupyter labextension install @jupyter-widgets/jupyterlab-manager
   jupyter contrib nbextension install --sys-prefix
