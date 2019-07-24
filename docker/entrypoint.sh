e2563ffc7f150925d5d25e861e47de7b0af94d79
Casper da Costa-Luis <casper.dcl@physics.org>
2019-07-24 11:32:17 +0100

    entrypoint fix, build optimisation

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Add local user
 # Either use runtime USER_ID:GROUP_ID or fallback 1000:1000
 
@@ -22,12 +21,12 @@ cd /
 #mv $OLD_HOME $HOME
 #cd $HOME
 
-echo "$UID:$GID Creating and switching to: $mainUser:$USER_ID:$GROUP_ID"
-# groupadd -g $GROUP_ID -o -f $mainUser
-addgroup --quiet --system --gid "$GROUP_ID" sirf
-useradd --system --shell /bin/bash -u $USER_ID -o -c "" -m -k $OLD_HOME \
-   -g sirf -G sudo $mainUser \
-   -p $(echo virtual | openssl passwd -1 -stdin)
+echo "$UID:$GID Creating and switching to: $mainUser:sirf ($USER_ID:$GROUP_ID)"
+groupadd --non-unique --system --gid "$GROUP_ID" sirf
+#addgroup --quiet --system --gid "$GROUP_ID" sirf
+useradd --system --shell /bin/bash -u $USER_ID --non-unique -m --skel $OLD_HOME \
+   -N -g sirf -G sudo \
+   -p "$(echo virtual | openssl passwd -1 -stdin)" "$mainUser"
 #adduser --quiet --system --shell /bin/bash \
 #  --no-create-home --home /home/"$mainUser" \
 #  --ingroup sirf --uid "$USER_ID" "$mainUser"
@@ -35,11 +34,11 @@ echo "$mainUser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/"$mainUser"
 
 for i in /opt/* "$HOME"; do
   if [ -d "$i" ]; then
-    chown -R $mainUser "$i"
-    chgrp -R $mainUser "$i"
+    chown -R $mainUser:sirf "$i"
   fi
 done
 
+cd $HOME
 if [ "$@" = "/usr/local/bin/service.sh" ]; then
   bash /usr/local/bin/service.sh
 else
