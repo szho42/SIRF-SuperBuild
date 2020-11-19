55e7a97eb1c4d511f8eef20cbab12befa70e4ecc
Shenjun Zhong <zhongsj52@gmail.com>
2020-11-19 16:38:10 +1100

    Update user_python-ubuntu.sh
    update to install latest python 3 version and pip

--- docker/user_python-ubuntu.sh
+++ docker/user_python-ubuntu.sh
@@ -8,7 +8,7 @@ PYTHON="${2:-miniconda}"
 case "$PYTHON" in
 miniconda)
   # miniconda
-  curl https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh > miniconda.sh
+  curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh > miniconda.sh
   echo -e "\nyes\n${INSTALL_DIR}" | bash miniconda.sh
   rm miniconda.sh
   source "$INSTALL_DIR"/bin/activate
@@ -34,7 +34,7 @@ esac
 
 # Python (runtime)
 if [ -f requirements.txt ]; then
-  pip install -U -r requirements.txt
+  pip3 install -U -r requirements.txt
 fi
 
 if [ "$PYTHON" = "miniconda" ]; then
